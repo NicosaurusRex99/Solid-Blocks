7bdf56900c9a466cbd0fb61bffe67a441dfd56d2
Nicusha <nicosaurusrex99@gmail.com>
2022-10-03 04:43:16 +1030

    added default colours

--- src/main/java/nicusha/solidblocks/Main.java
+++ src/main/java/nicusha/solidblocks/Main.java
@@ -1,19 +1,14 @@
 package nicusha.solidblocks;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.client.Minecraft;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.*;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -22,7 +17,6 @@ import org.slf4j.Logger;
 
 import java.util.function.Supplier;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(Main.MODID)
 public class Main
 {
@@ -31,9 +25,22 @@ public class Main
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    public static final RegistryObject<Block> red = registerBlock("red", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_RED)));
-    public static final RegistryObject<Block> green = registerBlock("green", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GREEN)));
-    public static final RegistryObject<Block> blue = registerBlock("blue", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_BLUE)));
+    public static final RegistryObject<Block> white = registerBlock("white", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.TERRACOTTA_WHITE).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> orange = registerBlock("orange", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_ORANGE).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> magenta = registerBlock("magenta", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_MAGENTA).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> light_blue = registerBlock("light_blue", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_LIGHT_BLUE).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> yellow = registerBlock("yellow", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_YELLOW).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> light_green = registerBlock("light_green", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_LIGHT_GREEN).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> pink = registerBlock("pink", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_PINK).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> gray = registerBlock("gray", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GRAY).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> light_gray = registerBlock("light_gray", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_LIGHT_GRAY).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> cyan = registerBlock("cyan", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_CYAN).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> purple = registerBlock("purple", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_PURPLE).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> blue = registerBlock("blue", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_BLUE).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> brown = registerBlock("brown", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_BROWN).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> green = registerBlock("green", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_GREEN).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> red = registerBlock("red", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_RED).strength(1.5F, 6.0F)));
+    public static final RegistryObject<Block> black = registerBlock("black", () -> new Block(BlockBehaviour.Properties.of(Material.STONE, MaterialColor.COLOR_BLACK).strength(1.5F, 6.0F)));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String registryName, Supplier<T> block) {
         RegistryObject<T> registeredBlock = BLOCKS.register(registryName, block);
@@ -59,4 +66,5 @@ public class Main
             return new ItemStack(red.get());
         }
     };
+
 }
